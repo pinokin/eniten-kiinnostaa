6c45d57e40e0c1b1e596278caa45f9a5ba5f58ad
Tuija <til.utilities@proton.me>
2026-01-30 16:37:39 +0200

    Added baseUrl to links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://pinokin.github.io',
+	base: '/eniten-kiinnostaa',
 	integrations: [mdx(), sitemap()],
 });
